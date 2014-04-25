f7c1b660314a07f497bef18e3c451b609a356645
floweryada <floweryada@gmail.com>
2014-04-25 08:34:15 +0300

    1.0

---
+++ users.cpp
@@ -0,0 +1,11 @@
+#include "users.h"
+#include <iostream>
+
+using namespace std;
+
+Data enterData (Data data)
+{
+    cout << "Please, enter the basic data: \n";
+    cin >> data;
+    return data;
+}
